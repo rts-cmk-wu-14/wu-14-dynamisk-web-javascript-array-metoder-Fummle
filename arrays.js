4ed74864eba2e774c63eb5d22bb7f7b315def9d6
fummle <fummelybum@gmail.com>
2025-08-25 12:32:48 +0200

    opgave 4b færdig

--- arrays.js
+++ arrays.js
@@ -39,7 +39,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Check om der i catsArray er en kat som hedder "Salem".
     // Udskriv katten i konsollen hvis den findes og ellers "undefined".
     // console.log("Opgave 4b", ...)
-    
+    console.log("Opgave 4b", catsArray.includes("Salem") ? "Salem" : undefined);
     //     Opgave 5
     // Med udgangspunkt i numbersArray, skal du oprette et nyt array, som indeholder alle tallene i arrayet ganget med 3.
     // Udskriv det nye array i konsollen:
